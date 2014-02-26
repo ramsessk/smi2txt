914969dc1a64d0c1f57326487d96a4c928f0b7a0
steven <ramsessk@gmail.com>
2014-02-26 22:35:03 +0800

    added missed setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from smi2txt_ezsetup import use_setuptools
+use_setuptools()
+from setuptools import setup
+
+#from setuptools import setup
+
+setup(name='mp4encode',
+      version='1.1.3',
+      author='Steven',
+      author_email='ramsessk@gmail.com',
+      url='https://ramsessk@bitbucket.org/ramsessk/mp4encode.git',
+      description='smi to srt, smi to txt converter',
+      long_description=open('readme.md').read(),
+      packages = ['smi2txt'],
+      package_dir={'smi2txt': 'src'},
+      package_data={'smi2txt': ['smi2txt.py', '*.py']}
+      )
